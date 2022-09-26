8cd2cd0095ca4adbc7b8aaa69a7c4383d0153046
Jose Pirir <josepirirromero@gmail.com>
2022-09-26 08:08:08 -0600

    test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const userController = require('./controllers/user.controller');
 const app = require('./app');
 const port = app.set('port', process.env.PORT || 3200);
+console.log(port);
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb+srv://josepirir:55835439romero@cluster0.otmlw8j.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true}) /*cluster atlas*/
