4a61ba7e7be2d291158a423dd2126b5252e8e4d0
Jose Pirir <josepirirromero@gmail.com>
2022-09-26 23:05:30 -0600

    feature/Heroku

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const mongoose = require('mongoose');
-const port = '3200';
 const userController = require('./controllers/user.controller');
 const app = require('./app');
-
+const port = process.env.PORT || 3200;
+const nodeEnv = process.env.NODE_ENV;
+const mySetting = process.env.MY_SETTING;
+//332ebee62a45180d8a2017e367b6dd826cb5bb65
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/pyme', {useNewUrlParser: true})
+mongoose.connect('mongodb+srv://josepirir:55835439romero@cluster0.otmlw8j.mongodb.net', {useNewUrlParser: true})
     .then(()=>{
     console.log('Conectado a la base de Datos :D');
     userController.createAdmin();
