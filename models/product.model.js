f17ea348f2c964283e17428683e44bb1aefd724a
Jose Pirir <josepirirromero@gmail.com>
2022-10-10 16:11:55 -0600

    productSchema update

--- models/product.model.js
+++ models/product.model.js
@@ -5,15 +5,15 @@ const Schema = mongoose.Schema;
 
 var productSchema = Schema({
     name: String,
+    descripcion: String,
+    brand: String,
+    size : String,
     price: Number,
+    available: String,
+    gender: String,
     image1: String,
     image2: String,
-    image3: String,
-    available: String,
-    descripcion: String,
-    size : String,
-    brand: String,
-    gender: String
+    image3: String
 });
 
 module.exports = mongoose.model('product', productSchema);
