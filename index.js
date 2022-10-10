df30fc889b4cc9c66928fc2dfd01e49d2e8f549a
Jose Pirir <josepirirromero@gmail.com>
2022-10-10 16:14:59 -0600

    cambios

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 const userController = require('./controllers/user.controller');
 const app = require('./app');
 const port = process.env.PORT || 3200;
-const nodeEnv = process.env.NODE_ENV;
-const mySetting = process.env.MY_SETTING;
 //332ebee62a45180d8a2017e367b6dd826cb5bb65
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb+srv://josepirir:55835439romero@cluster0.otmlw8j.mongodb.net', {useNewUrlParser: true})
